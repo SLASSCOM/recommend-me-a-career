8272dad4d1d488c74823f14b1bc3803a49287238
OpenAshan <hmajds@hotmail.com>
2023-09-20 17:30:05 +0530

    Front end part is done.

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,17 +3,17 @@ import styles from "./header.css";
 
 export default function Header() {
     return (
-        <header className={styles.header}>
-            <div className={styles.logo}>
+        <header className="header">
+            <div className="logo">
                 <a href="https://open.uom.lk/index.html">
-                    <a className={styles.navbarBrand}>
+                    <a className="navbarBrand">
                         <img src="https://open.uom.lk/assets/images/logo.png" alt="" />
                     </a>
                 </a>
             </div>
-            <div>
-                <a href="https://open.uom.lk/recommend.html" className={styles.coursesLink}>Courses</a>
-                <a href="https://open.uom.lk/lms/my" className={styles.loginBtn}>Login</a>
+            <div className="headerRight">
+                <a href="https://open.uom.lk/recommend.html" className="coursesLink">Courses</a>
+                <a href="https://open.uom.lk/lms/my" className="loginBtn">Login</a>
             </div>
         </header>
     );
--- src/app/page.js
+++ src/app/page.js
@@ -1,176 +1,151 @@
 "use client";
 import React, { useState } from "react";
 import Header from "./components/Header";
-import quizData from "../../public/questions";
-import Link from "next/link";
+import quizData from "./questions";
+import careersData from "./careers.json"; // Import the careers list
 import "./globals.css";
+import Script from "next/script";
 
 const Page = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
-  const [careerLists, setCareerLists] = useState([]);
+  const [userAnswers, setUserAnswers] = useState(Array(quizData.Questions.length).fill(null));
+  const [recommendedCareers, setRecommendedCareers] = useState([]);
   const [showLog, setShowLog] = useState(false);
   const resultCount = 10;
 
   const handleNextButtonClick = () => {
     if (currentQuestion < quizData.Questions.length - 1) {
-      setCurrentQuestion(currentQuestion + 1);
-    } else {
+      if (userAnswers[currentQuestion] !== null) {
+        setCurrentQuestion(currentQuestion + 1);
+      } else {
+        // Handle the case where the user hasn't selected an answer
+      }
+    } else if (currentQuestion === quizData.Questions.length - 1) {
+      // If on the 11th question, calculate results
       calculateResults();
     }
   };
 
-  const goToPreviousQuestion = () => {
+  const handleBackButtonClick = () => {
     if (currentQuestion > 0) {
       setCurrentQuestion(currentQuestion - 1);
     }
   };
 
   const calculateResults = () => {
-    const formData = new FormData(document.getElementById("quiz-form"));
-    const userChoices = Array.from(formData.values());
-
     const careerCount = {};
 
-    userChoices.forEach((choice) => {
-      const careers =
-        quizData.Questions[currentQuestion - 1].Answers[choice].Careers;
-      careerLists[currentQuestion] = careers;
-      careers.forEach((career) => {
-        careerCount[career] = (careerCount[career] || 0) + 1;
-      });
+    userAnswers.forEach((choice, index) => {
+      if (choice !== null) {
+        const careers = quizData.Questions[index].Answers[choice].Careers;
+        careers.forEach((career) => {
+          careerCount[career] = (careerCount[career] || 0) + 1;
+        });
+      }
     });
 
-    const recommendedCareers = Object.entries(careerCount)
+    const sortedCareers = Object.entries(careerCount)
       .sort((a, b) => b[1] - a[1])
-      .slice(0, resultCount);
+      .map((item) => item[0]);
 
-    setCareerLists([...careerLists]);
-    displayResult();
-    if (showLog) {
-      displayLog();
-    }
+    const recommendedCareers = sortedCareers.slice(0, resultCount);
+
+    setRecommendedCareers(recommendedCareers);
+    setShowLog(false); // Hide the log
   };
 
-  const displayResult = () => {
-    const resultEl = document.getElementById("result");
-    const careerListEl = document.getElementById("careerList");
-
-    resultEl.classList.remove("hidden");
-    careerListEl.innerHTML = "";
-
-    careerLists[careerLists.length - 1].forEach((career, index) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = career;
-      careerListEl.appendChild(listItem);
-    });
-  };
-
-  const displayLog = () => {
-    const careerLogEl = document.getElementById("log");
-    careerLogEl.classList.remove("hidden");
-
-    careerLogEl.innerHTML = "";
-
-    careerLists.forEach((list, index) => {
-      const listEl = document.createElement("ul");
-      list.forEach((career) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = career;
-        listEl.appendChild(listItem);
-      });
-      const label = document.createElement("label");
-      label.textContent =
-        "Q:" + index + " - " + quizData.Questions[index - 1].Caption;
-      careerLogEl.appendChild(label);
-      careerLogEl.appendChild(listEl);
-    });
-  };
-
-  const handleShowLogClick = () => {
-    setShowLog(!showLog);
-    displayLog();
+  const handleAnswerChange = (event) => {
+    const updatedUserAnswers = [...userAnswers];
+    updatedUserAnswers[currentQuestion] = parseInt(event.target.value);
+    setUserAnswers(updatedUserAnswers);
   };
 
   const questionData = quizData.Questions[currentQuestion];
 
   return (
     <div>
-        
       <Header />
       <div className="container">
         <h1>Which tech job is best for my personality?</h1>
-        <form id="quiz-form" className="quiz-container">
-          <div className="question-container">
-            <h3>Question {currentQuestion + 1}</h3>
-            <h2>{questionData.Caption}</h2>
-            {questionData.Answers.map((answer, index) => (
-              <div key={index} className="answer-container">
-                <input
-                  type="radio"
-                  name={`question${currentQuestion}`}
-                  id={`question${currentQuestion}answer${index}`}
-                  value={index}
-                />
-                <label
-                  htmlFor={`question${currentQuestion}answer${index}`}
+        <div id="quiz-form" className="quiz-container">
+          {recommendedCareers.length === 0 && (
+            <div className="question-container">
+              <h3>Question {currentQuestion + 1}</h3>
+              <h2>{questionData.Caption}</h2>
+              {questionData.Answers.map((answer, index) => (
+                <div key={index} className="answer-container">
+                  <input
+                    type="radio"
+                    name={`question${currentQuestion}`}
+                    id={`question${currentQuestion}answer${index}`}
+                    value={index}
+                    checked={userAnswers[currentQuestion] === index}
+                    onChange={handleAnswerChange}
+                  />
+                  <label htmlFor={`question${currentQuestion}answer${index}`}>
+                    {answer.Caption}
+                  </label>
+                </div>
+              ))}
+              <div className="button-container">
+                <button
+                  id="back"
+                  className={`action-btn ${
+                    currentQuestion === 0 ? "disabled" : ""
+                  }`}
+                  onClick={handleBackButtonClick}
                 >
-                  {answer.Caption}
-                </label>
+                  Back
+                </button>
+                <button
+                  id="next"
+                  className="action-btn"
+                  onClick={handleNextButtonClick}
+                >
+                  {currentQuestion === quizData.Questions.length - 1
+                    ? "Calculate Results"
+                    : "Next"}
+                </button>
               </div>
-            ))}
-                      <div className="button-container">
-          <button
-            id="back"
-            className={`action-btn ${
-              currentQuestion === 0 ? "disabled" : ""
-            }`}
-            onClick={goToPreviousQuestion}
-          >
-            Back
-          </button>
-          <button
-            id="next"
-            className="action-btn"
-            onClick={handleNextButtonClick}
-          >
-            Next
-          </button>
-        </div>
-          </div>
-        </form>
+            </div>
+          )}
 
-        <div className="result hidden">
-          <h2>Recommended careers for you in priority order</h2>
-          <ol id="careerList"></ol>
-          <button
-            id="tryAgain"
-            className="action-btn"
-            onClick={() => {
-              setCurrentQuestion(0);
-              setUserAnswers([]);
-              setCareerLists([]);
-              displayQuestion();
-            }}
-          >
-            Try Again
-          </button>
+          {recommendedCareers.length > 0 && (
+            <div className="result">
+              <h2>Recommended careers for you in priority order</h2>
+              <ol id="careerList">
+                {recommendedCareers.map((career, index) => (
+                  <li key={index}>{career}</li>
+                ))}
+              </ol>
+              <button
+                id="tryAgain"
+                className="action-btn"
+                onClick={() => {
+                  setCurrentQuestion(0);
+                  setUserAnswers(Array(quizData.Questions.length).fill(null));
+                  setRecommendedCareers([]);
+                }}
+              >
+                Try Again
+              </button>
+            </div>
+          )}
         </div>
-        {/* <div className="log hidden">
-          <h2>Answer Log</h2>
-          <ol id="log"></ol>
-        </div>
+{/* 
         <button
           id="showLogBtn"
-          className="action-btn"
+          className={`action-btn ${
+            recommendedCareers.length > 0 || showLog ? "hidden" : ""
+          }`}
           onClick={handleShowLogClick}
         >
           Show Answer Log
-        </button>
-      </div> */}
+        </button> */}
       </div>
     </div>
   );
 };
 
 export default Page;
+
