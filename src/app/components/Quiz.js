cba0545289d7365fc5e1402cdaf32cc1d520ece7
AshanSenadheera <144100387+AshanSenadheera@users.noreply.github.com>
2023-10-02 15:18:15 +0530

    Broken down into components.
    Broken down into components. 
    
    1. Moved the part of Page.js that displays the questions and handles the question API to Quiz.js.
    2. The styles.css stylesheet was included as quiz.css for Quiz.js.

---
+++ src/app/components/Quiz.js
@@ -0,0 +1,194 @@
+import React, { useState, useEffect } from "react";
+import quizData from "./questions";
+import careersData from "./careers.json";
+import links from "./links";
+import "./quiz.css";
+
+export default function Quiz() {
+  const [session, setSession] = useState(generateSessionId());
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userAnswers, setUserAnswers] = useState(Array(quizData.Questions.length).fill(null));
+  const [recommendedCareers, setRecommendedCareers] = useState([]);
+  const [showLog, setShowLog] = useState(false);
+  const resultCount = 10;
+
+  // Function to generate a unique sessionId
+  function generateSessionId() {
+    return "OUOMLK" + Math.random().toString(36).substring(2) + "OpenUomLk" + Date.now().toString(36) + "2023" + Date.now().toString(18) +  + Math.random().toString(36).substring(3) + "SID" ;
+  }
+
+  useEffect(() => {
+    // Set the initial session ID when the component mounts
+    setSession(generateSessionId());
+  }, []);
+
+  // Function to call the first API
+  async function callInsertAnswerAPI(question, answer) {
+    const url = `https://script.google.com/macros/s/AKfycbwCaeWzrO5bdVJ29WKAekKHJJcmqAWiRzZT0FCtkHlS0xkhaDQiSnoH7NYStlwVSBzvVw/exec?action=insertAnswer&sessionId=${session}&question=${question}&answer=${answer}`;
+
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        // API call was successful, handle the response if needed
+      } else {
+        // Handle errors if the API call fails
+      }
+    } catch (error) {
+      // Handle network errors
+    }
+  }
+
+  // Function to call the second API
+  async function callInsertRecommendationsAPI(recommendations) {
+    const url = `https://script.google.com/macros/s/AKfycbwCaeWzrO5bdVJ29WKAekKHJJcmqAWiRzZT0FCtkHlS0xkhaDQiSnoH7NYStlwVSBzvVw/exec?action=insertRecommendations&sessionId=${session}&recommendation=${recommendations.join('&recommendation=')}`;
+
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        // API call was successful, handle the response if needed
+      } else {
+        // Handle errors if the API call fails
+      }
+    } catch (error) {
+      // Handle network errors
+    }
+  }
+
+  function handleNextButtonClick() {
+    if (currentQuestion < quizData.Questions.length - 1) {
+      if (userAnswers[currentQuestion] !== null) {
+        const question = quizData.Questions[currentQuestion].Caption;
+        const answer = quizData.Questions[currentQuestion].Answers[userAnswers[currentQuestion]].Caption;
+        callInsertAnswerAPI(question, answer);
+        setCurrentQuestion(currentQuestion + 1);
+      } else {
+        // Handle the case where the user hasn't selected an answer
+      }
+    } else if (currentQuestion === quizData.Questions.length - 1) {
+      calculateResults();
+    }
+  }
+
+  function handleBackButtonClick() {
+    if (currentQuestion > 0) {
+      setCurrentQuestion(currentQuestion - 1);
+    }
+  }
+
+  function calculateResults() {
+    const careerCount = {};
+
+    userAnswers.forEach((choice, index) => {
+      if (choice !== null) {
+        const careers = quizData.Questions[index].Answers[choice].Careers;
+        careers.forEach((career) => {
+          careerCount[career] = (careerCount[career] || 0) + 1;
+        });
+      }
+    });
+
+    const sortedCareers = Object.entries(careerCount)
+      .sort((a, b) => b[1] - a[1])
+      .map((item) => item[0]);
+
+    const recommendedCareers = sortedCareers.slice(0, resultCount);
+
+    setRecommendedCareers(recommendedCareers);
+
+    callInsertRecommendationsAPI(recommendedCareers);
+
+    setShowLog(false);
+  }
+
+  function handleAnswerChange(event) {
+    const updatedUserAnswers = [...userAnswers];
+    updatedUserAnswers[currentQuestion] = parseInt(event.target.value);
+    setUserAnswers(updatedUserAnswers);
+  }
+
+  const questionData = quizData.Questions[currentQuestion];
+  const questionNumber = currentQuestion + 1;
+  const totalQuestions = quizData.Questions.length;
+  const showBackButton = currentQuestion > 0;
+  const isNextButtonDisabled = userAnswers[currentQuestion] === null;
+
+  return (
+    <div>
+      <div className="container">
+        {!recommendedCareers.length > 0 && (
+          <h1 className="mainHeading">Which tech job is best for my personality?</h1>
+        )}
+        <div id="quiz-form" className="quiz-container">
+          {recommendedCareers.length === 0 ? (
+            <div className="question-container">
+              <h3>Question {questionNumber} of {totalQuestions}</h3>
+              <h2>{questionData.Caption}</h2>
+              {questionData.Answers.map((answer, index) => (
+                <div key={index} className="answer-container">
+                  <input
+                    type="radio"
+                    name={`question${currentQuestion}`}
+                    id={`question${currentQuestion}answer${index}`}
+                    value={index}
+                    checked={userAnswers[currentQuestion] === index}
+                    onChange={handleAnswerChange}
+                  />
+                  <label htmlFor={`question${currentQuestion}answer${index}`}>
+                    {answer.Caption}
+                  </label>
+                </div>
+              ))}
+              <div className="button-container">
+                {showBackButton && (
+                  <button
+                    id="back"
+                    className={`action-btn ${
+                      currentQuestion === 0 ? "disabled" : ""
+                    }`}
+                    onClick={handleBackButtonClick}
+                  >
+                    Back
+                  </button>
+                )}
+                <button
+                  id="next"
+                  className="action-btn"
+                  onClick={handleNextButtonClick}
+                  disabled={isNextButtonDisabled}
+                >
+                  {currentQuestion === quizData.Questions.length - 1
+                    ? "Calculate Results"
+                    : "Next"}
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="result">
+              <h2>Recommended careers for you in priority order</h2>
+              <ol id="careerList">
+                {recommendedCareers.map((career, index) => (
+                  <li key={index} className={index < 3 ? "highlighted" : ""}>
+                    <a href={links[career]} target="_blank" rel="noopener noreferrer">
+                      {career}
+                    </a>
+                  </li>
+                ))}
+              </ol>
+              <button
+                id="tryAgain"
+                className="action-btn"
+                onClick={() => {
+                  setCurrentQuestion(0);
+                  setUserAnswers(Array(quizData.Questions.length).fill(null));
+                  setRecommendedCareers([]);
+                }}
+              >
+                Try Again
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/questions.js
@@ -0,0 +1,337 @@
+const quizData = {
+    "Questions": [
+        {
+            "Caption": "How good are you with numbers?",
+            "Answers": [
+                {
+                    "Caption": "I love numbers and complex math",
+                    "Careers": [
+                        "Data Scientist",
+                        "Full Stack Developer",
+                        "Cloud Developer",
+                        "Python Programmer"
+                    ]
+                },
+                {
+                    "Caption": "I can manage basic math and simple formulas",
+                    "Careers": [
+                        "Software Automation Engineer",
+                        "DevOps Professional",
+                        "Mobile Developer",
+                        "Security Professional"                        
+                    ]
+                },
+                {
+                    "Caption": "I struggle with maths and complex data",
+                    "Careers": [
+                        "UX Professional",
+                        "Software Tester",
+                        "Web Developer",
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you handle stress?",
+            "Answers": [
+                {
+                    "Caption": "I struggle under pressure and need regular breaks",
+                    "Careers": [
+                        "Data Scientist",
+                        "Software Tester",
+                        "Software Automation Engineer",
+                        "Python Programmer",
+                    ]
+                },
+                {
+                    "Caption": "I can easily stay productive under pressure",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "DevOps Professional",
+                        "Security Professional",
+                        "Cloud Developer",
+                    ]
+                },
+                {
+                    "Caption": "I ask for help from others when under pressure",
+                    "Careers": [
+                        "Mobile Developer",
+                        "UX Professional",
+                        "Web Developer"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you solve problems?",
+            "Answers": [
+                {
+                    "Caption": "I break them into smaller parts and solve",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "Data Scientist",
+                        "Cloud Developer",
+                        "Python Programmer"
+                    ]
+                },
+                {
+                    "Caption": "I try different ways and get opinion from others",
+                    "Careers": [
+                        "UX Professional",
+                        "Web Developer",
+                        "Mobile Developer"
+                    ]
+                },
+                {
+                    "Caption": "I follow known procedures to solve a problem",
+                    "Careers": [
+                        "DevOps Professional",
+                        "Software Automation Engineer",
+                        "Security Professional",
+                        "Software Tester"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you learn?",
+            "Answers": [
+                {
+                    "Caption": "I learn by doing things hands-on by myself",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "Python Programmer",
+                        "Cloud Developer",
+                        "Data Scientist"
+                    ]
+                },
+                {
+                    "Caption": "I learn by reading and studying available content",
+                    "Careers": [
+                        "Software Automation Engineer",
+                        "DevOps Professional",
+                        "Security Professional",
+                        "Software Tester"
+                    ]
+                },
+                {
+                    "Caption": "I learn from others by working together",
+                    "Careers": [
+                        "Mobile Developer",
+                        "Web Developer",
+                        "UX Professional",
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you make decisions?",
+            "Answers": [
+                {
+                    "Caption": "I ask others and collaborate on decision making",
+                    "Careers": [
+                        "Mobile Developer",
+                        "UX Professional",
+                        "Web Developer"
+                    ]
+                },
+                {
+                    "Caption": "I make decisions quickly and independently",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "Data Scientist",
+                        "Security Professional",
+                        "Cloud Developer"
+                    ]
+                },
+                {
+                    "Caption": "I want more infomation to consider all options",
+                    "Careers": [
+                        "DevOps Professional",
+                        "Software Tester",
+                        "Software Automation Engineer",
+                        "Python Programmer"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you multitask?",
+            "Answers": [
+                {
+                    "Caption": "I can handle multiple tasks and switch between them easily",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "DevOps Professional",
+                        "Cloud Developer",
+                        "Mobile Developer",
+                        "Web Developer",
+                    ]
+                },
+                {
+                    "Caption": "I like to prioritize and focus on one task at a time",
+                    "Careers": [
+                        "Python Programmer",
+                        "Security Professional",
+                        "Data Scientist",
+                        "Software Automation Engineer",
+                        "UX Professional",
+                        "Software Tester"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you work in teams?",
+            "Answers": [
+                {
+                    "Caption": "I like to privide leadership the team when possible",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "Cloud Developer",
+                        "Data Scientist"
+                    ]
+                },
+                {
+                    "Caption": "I like to play my part in the team and contribute",
+                    "Careers": [
+                        "Web Developer",
+                        "DevOps Professional",
+                        "UX Professional",
+                        "Mobile Developer",
+                        "Software Tester"
+                    ]
+                },
+                {
+                    "Caption": "I like to work independently to avoid distractions",
+                    "Careers": [
+                        "Python Programmer",
+                        "Software Automation Engineer",
+                        "Security Professional"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "What is is your risk taking appetite?",
+            "Answers": [
+                {
+                    "Caption": "I love taking risks and try new solutions",
+                    "Careers": [
+                        "Mobile Developer",
+                        "Full Stack Developer",
+                        "Cloud Developer",
+                        "UX Professional"
+                    ]
+                },
+                {
+                    "Caption": "I carefully take calculated risks but always look at practicality",
+                    "Careers": [
+                        "DevOps Professional",
+                        "Python Programmer",
+                        "Web Developer",
+                        "Data Scientist"
+                    ]
+                },
+                {
+                    "Caption": "I stick to proven approaches and do not like to take risks",
+                    "Careers": [
+                        "Software Tester",
+                        "Software Automation Engineer",
+                        "Security Professional"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "What type of work you enjoy most?",
+            "Answers": [
+                {
+                    "Caption": "I'm a perfectionist and like to correct even minor details",
+                    "Careers": [
+                        "Python Programmer",
+                        "Software Tester",
+                        "Security Professional",
+                        "DevOps Professional",
+                        "Software Automation Engineer"
+                    ]
+                },
+                {
+                    "Caption": "I like challenging problems and do NOT enjoy repetitive tasks",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "Cloud Developer",
+                        "Mobile Developer",
+                        "Data Scientist"
+                    ]
+                },
+                {
+                    "Caption": "I like to work on tasks where I can colaborate and understand others",
+                    "Careers": [
+                        "Web Developer",
+                        "UX Professional" 
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you look at mistakes and errors?",
+            "Answers": [
+                {
+                    "Caption": "I like to look at them as learning opportunities to improve",
+                    "Careers": [
+                        "Mobile Developer",
+                        "UX Professional",
+                        "Full Stack Developer",
+                        "Cloud Developer",
+                        "Web Developer"
+                    ]
+                },
+                {
+                    "Caption": "I like to avoid them at all costs and correct immediately if occured",
+                    "Careers": [
+                        "Security Professional",
+                        "Software Automation Engineer",
+                        "Software Tester",
+                        "DevOps Professional",
+                        "Data Scientist",
+                        "Python Programmer"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How good are you with business and enterprise concepts?",
+            "Answers": [
+                {
+                    "Caption": "I love learning about different industries and know a lot about them",
+                    "Careers": [
+                        "Data Scientist",
+                        "UX Professional"
+                    ]
+                },
+                {
+                    "Caption": "I have some interest about businesses and can understand them",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "Web Developer",
+                        "Mobile Developer",
+                        "Software Tester",
+                        "Cloud Developer"
+                    ]
+                },
+                {
+                    "Caption": "I have limited knowledge on businesses and not much of an interest",
+                    "Careers": [
+                        "Security Professional",
+                        "Software Automation Engineer",
+                        "DevOps Professional",
+                        "Python Programmer",
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+export default quizData;
