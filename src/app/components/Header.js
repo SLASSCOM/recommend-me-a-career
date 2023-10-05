080048732858b7aa4a4552d9f70a6a14c918e5fd
Hasith Yaggahavita <hasith@gmail.com>
2023-10-05 13:32:50 +0530

    fixed warnings from react

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -41,7 +41,7 @@ export default function Header() {
       
       {/* Mobile Hamburger Menu */}
       <input type="checkbox" id="menu-toggle" className="menu-toggle" />
-      <label for="menu-toggle" class="menu-label">&#9776;
+      <label htmlFor="menu-toggle" className="menu-label">&#9776;
       </label>
       <div className="menu">
         <Link href="https://open.uom.lk/recommend.html" className="courses-link">
